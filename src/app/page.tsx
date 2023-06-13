01ba51f0c25caff81e7f4952bbd1c30063ea666f
Maiterney <43651018+Maiterney@users.noreply.github.com>
2023-06-12 23:35:58 -0300

    Fix - Error login data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import { InitPage } from "@/components/Init"
+import { api } from "@/utils"
 import { cookies } from "next/dist/client/components/headers"
+import { redirect } from "next/navigation"
 
-export default function Home() {
+export default async function Home() {
   const cookie = cookies()
   const token = cookie.get('nextAuth.token')?.value
+  await api.get('/user/installations').then(res => { 
+    console.log(res?.data?.installations?.results) 
+    redirect(`/${res.data.installations.results[0].number}/dashboard`)
+  }).catch(err => { 
+      console.log(err); 
+      redirect('https://login.woltz.com.br')
+  })
   
   return <InitPage token={token}/>
 }
