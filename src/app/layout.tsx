6c827273d68f78809a10cdc14648bdc843103080
Maiterney <43651018+Maiterney@users.noreply.github.com>
2023-06-12 23:20:55 -0300

    Fix- redirecionamento

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,18 +22,26 @@ export default async function RootLayout({ children, }: { children: React.ReactN
   const userEmail = cookie.get('nextAuth.email')?.value
   if(token) {
     api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+    api.get('/user/installations').then(res => { 
+      console.log(res?.data?.installations?.results) 
+      redirect(`/${res.data.installations.results[0].number}/dashboard`)
+    }).catch(err => { 
+        console.log(err); 
+        redirect('https://login.woltz.com.br')
+    })
     
-    await api.post('/authenticate/refresh-token', { email: userEmail, refresh_token: refreshToken }).then(res => {
+    /* await api.post('/authenticate/refresh-token', { email: userEmail, refresh_token: refreshToken }).then(res => {
       console.log('success')
     }).catch(err => {
       console.log('aqui')
       redirect('http://localhost:3001')
       // redirect('https://login.woltz.com.br')
-    })
+    }) */
     
   } else {
-    redirect('http://localhost:3001')
-    // redirect('https://login.woltz.com.br')
+    // redirect('http://localhost:3001')
+    redirect('https://login.woltz.com.br')
   }
   return (
     <html lang="en">
